8d651e449fa8c2f71ea10cd60a225c3a854ae677
Jobson Batista <jobson.silva@dcx.ufpb.br>
2020-11-27 07:58:03 -0300

    Alterando os JOINs

--- JOINs.sql
+++ JOINs.sql
@@ -1,21 +1,14 @@
---USUARIOS QUE POSSUEM COMENTARIOS ANONIMOS NA EMPRESA speakrow1
-SELECT e.nome, c.id_usuario 
+SELECT *
 FROM empresa as e
 INNER JOIN comentario as c
-on e.id = c.id_empresa
-WHERE c.anonimo = 1 AND e.nome = 'speakrow1';
+on e.id = c.id_empresa;
 
-
-SELECT e.nome, c.id_usuario 
+SELECT *
 FROM empresa as e
 LEFT JOIN comentario as c
 on e.id = c.id_empresa;
 
-SELECT e.nome, c.id_usuario 
+SELECT * 
 FROM empresa as e
 RIGHT JOIN comentario as c
 on e.id = c.id_empresa;
-
-
-SELECT * FROM empresa;
-SELECT * FROM comentario;
